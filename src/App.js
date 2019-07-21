a1dc7e8645aaad732f2af150ec73c8991f367bc3
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2019-07-21 10:57:04 +0000

    Add basic page structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            'images': null
+        };
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <main className="container-fluid">
+                <header className="row">
+                    <div className="col"><h1>Nasa Images</h1></div>
+                </header>
+                <section className="row">
+                    <Router>
+                        <nav className="col-4">
+                        </nav>
+                        <article className="col">
+                        </article>
+                    </Router>
+                </section>
+            </main>
+        );
+    }
 }
 
 export default App;
